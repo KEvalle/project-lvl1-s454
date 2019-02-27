97c432c80de411b9d174fd4d7c728c63a3a49775
Karantir <karantir@mail.ru>
2019-02-27 15:15:10 +0300

    task 2 update

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import readlineSync from 'readline-sync';
 
-export default (a) => {
+export default () => {
+console.log(`Welcome to the Brain Games!`);
 const actual = readlineSync.question('May I have your name?: ');
 console.log(`Hello, ${actual}!`);
 };
