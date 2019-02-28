63d6e159c05f103b992683392fbb656d8b76af93
Karantir <karantir@mail.ru>
2019-02-28 23:04:38 +0300

    task 4

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 # project-lvl1-s454
 [![Maintainability](https://api.codeclimate.com/v1/badges/bd2699c0cd68c60a132e/maintainability)](https://codeclimate.com/github/KEvalle/project-lvl1-s454/maintainability)
 [![Build Status](https://travis-ci.org/KEvalle/project-lvl1-s454.svg?branch=master)](https://travis-ci.org/KEvalle/project-lvl1-s454)
+[![asciicast](https://asciinema.org/a/D4hYJL3pvA1tJ6JMvTQgnyf5M.svg)](https://asciinema.org/a/D4hYJL3pvA1tJ6JMvTQgnyf5M)
--- src/index.js
+++ src/index.js
@@ -1,7 +1,27 @@
 import readlineSync from 'readline-sync';
 
-export default () => {
+export const askName = () => {
   console.log('Welcome to the Brain Games!');
   const actual = readlineSync.question('May I have your name?: ');
   console.log(`Hello, ${actual}!`);
+};
+
+export const checkEven = () => {
+  console.log("Welcome to the Brain Games! \nAnswer 'yes' if number even otherwise answer 'no'.\n");
+  const Name = readlineSync.question('May I have your name?: ');
+  console.log(`Hello, ${Name}!\n`);
+
+  for (let counter = 1; counter <= 3; counter += 1) {
+    const rndNumber = Math.floor(Math.random() * 100);
+    const even = (rndNumber % 2 === 0);
+    const answer = readlineSync.question(`Question:${rndNumber}\nYou answer:`);
+
+    if ((even === true && answer === 'yes') || (even === false && answer === 'no')) {
+      console.log('Correct!');
+    } else {
+      console.log("'yes' is wrong answer ;(. Correct answer was 'no'.\nLet's try again, Bill!");
+      return;
+    }
+  }
+  console.log(`Congratulations, ${Name}!`);
 };
